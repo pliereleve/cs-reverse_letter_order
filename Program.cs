459d66896383845130f3b5eadd70f7cccab4fa46
pliereleve <asia.laskowska@gmail.com>
2021-11-04 22:12:27 +0100

    (C#) application that reverses letters of each word of

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = Console.ReadLine();
+            string[] words = tekst.Split(' ');      //do tablicy words zapisujemy słowa jako elementy
+            
+            foreach(string word in words)
+            {
+                char[] slowo = word.ToCharArray();      //poszczegolne słowa zapisujemy jako tablice charów zamiast string
+                Array.Reverse(slowo);                   //odwracamy kolejność elementów tablicy (odwracamy słowo)
+                Console.Write(slowo);
+                Console.Write(" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
